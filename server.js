172a465f99bb719ef317417c9521dc7373e3a8f0
umair0715 <ua071567@gmail.com>
2025-08-07 11:48:14 +0500

    db added

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+
+// CONNECT DB
+async function connectDB() {
+  const mongoUrl = process.env.DB_URL;
+  const dbName = process.env.DB_NAME;
+
+  if (!mongoUrl || !dbName) {
+    console.error('MONGO_URL and DB_NAME environment variables are required');
+    process.exit(1);
+  }
+  
+  try {
+    await mongoose.connect(mongoUrl, { dbName });
+    console.log('Database is connected');
+  } catch (err) {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  }
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
